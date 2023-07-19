9dc6606a52b9b408991525bfa90d5ae52d67a3c9
Maryamstar <mariammarshall02@gmail.com>
2023-07-19 14:23:52 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,11 +5,13 @@
  * Return: 0
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char i;
 
 	for (i = 'a' ; i <= 'z' ; i++)
-		_putchar(i);
+	{
+	_putchar(i);
+	}
 	_putchar('\n');
 }
